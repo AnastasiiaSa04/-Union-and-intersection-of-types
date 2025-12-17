0d13e9d9e6da820c068dad76ae7e368c0de18f87
Anastasiia <sulollarianastasiia@gmail.com>
2025-12-17 19:06:08 +0100

    Delete node_modules

--- src/controllers/carController.ts
+++ src/controllers/carController.ts
@@ -2,6 +2,7 @@ export type Engine = { type: string; horsepower: number };
 export type Car = { make: string; model: string; engine: Engine; year?: number };
 
 export const myCar: Car = {
+  
   make: "Mazda",
   model: "CX-5",
   engine: { type: "V8", horsepower: 300 },
@@ -12,4 +13,6 @@ export function printCarInfo(car: Car): void {
   console.log(`Car: ${car.make} ${car.model}`);
   console.log(`Engine: ${car.engine.type}, ${car.engine.horsepower} HP`);
   if (car.year) console.log(`Year: ${car.year}`);
+
+
 }
